d2ebd1c1ab44c8e66996109bb9dc853a400a4c14
Kiss Dániel <kissdani89@gmail.com>
2016-03-30 20:52:09 +0200

    Fix

--- ElasticBeanTester/src/main/java/com/daniekiss/hello/ebt/main/EBT.java
+++ ElasticBeanTester/src/main/java/com/daniekiss/hello/ebt/main/EBT.java
@@ -13,14 +13,12 @@ public class EBT extends HttpServlet{
 	private static final long serialVersionUID = -4169980837130701492L;
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
-		
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Calendar c = Calendar.getInstance();
 		
 		resp.setContentType("text/html;charset=utf-8");
         resp.setStatus(HttpServletResponse.SC_OK);
-		resp.getWriter().print("Hello World!\n-- " + c.getTimeZone().toZoneId() + ": " +  c.getTime());
+		resp.getWriter().print("Hello World!\n<br />-- " + c.getTimeZone().toZoneId() + ": " +  c.getTime());
 	}
 	
 }
